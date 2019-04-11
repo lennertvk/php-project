775f64085088f1297ba6a9182c976fde292341fb
Lennert Van Kerckhove <lennert.van.kerckhove@gmail.com>
2019-04-11 17:19:19 +0200

    pics loading on homepage

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 
-?>
+  $conn = mysqli_connect("mysql338.webhosting.be:3306", "ID280780_phpproject", "test1234", "ID280780_phpproject");
+  $result = mysqli_query($conn, "SELECT * FROM images");
 
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,5 +15,13 @@
 <body>
     <h1>this is the homepage</h1>
     <a href="upload.php">upload een foto</a>
+    <?php
+    while ($row = mysqli_fetch_array($result)) {
+      echo "<div id='img_div'>";
+      	echo "<img src='images/".$row['image']."' >";
+      	echo "<p>".$row['text']."</p>";
+      echo "</div>";
+    }
+  ?>
 </body>
 </html>
