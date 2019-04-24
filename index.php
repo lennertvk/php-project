46877f0ee967fd8403238eb989fb1b97a70ed61c
Miguel Lamm <49272909+MiguelLamm@users.noreply.github.com>
2019-04-24 17:10:26 +0200

    Search functie

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
   $conn = mysqli_connect("mysql338.webhosting.be:3306", "ID280780_phpproject", "test1234", "ID280780_phpproject");
   $result = mysqli_query($conn, "SELECT * FROM images WHERE minified = 1");
 
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,6 +15,12 @@
     <title>Homepage</title>
 </head>
 <body>
+<form action="search.php" method="get">
+    <label>search</label>
+    <input type="text" name="search" />
+    <input type="submit" value="Search" />
+    </form>
+
     <h1>this is the homepage</h1>
     <a href="upload.php">upload een foto</a>
     <br>
