00247631fbc869c68a6f937911a5135a4306f656
Miguel Lamm <49272909+MiguelLamm@users.noreply.github.com>
2019-05-05 20:04:50 +0200

    update search

--- search.php
+++ search.php
@@ -1,28 +1,32 @@
 <?php
-    
     require_once('bootstrap.php');
     $conn = Db::getInstance();
     $search = new Post();
     $searchResult=$search->search();
+    
 ?>
  
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Search results</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <link rel="stylesheet" type="text/css" href="style.css"/>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-<h2>Resultaten</h2>
+<?php include_once('includes/nav.inc.php')?>
+<h2>results</h2>
 <?php
     foreach($searchResult as $key):
 ?>
 <div>
 <?php 
-    echo "<img src='miniimages/".$key['image']."'width='150px'>";
+    echo "<img src='miniimages/".$key['image']."'width='250px'>";
 ?>
-<a href="search.detail.php?search=<?php echo $key['id'];?>"><?php echo $key['text']; ?></a>
+<a href="search.details.php?search=<?php echo $key['id'];?>"><?php echo $key['text']; ?></a>
 </div>
 <?php endforeach; ?>
 </body>
